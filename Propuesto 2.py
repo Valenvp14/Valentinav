783917182d06e9c78251d6e68770f1ee2b792dca
Valenvp14 <93169177+Valenvp14@users.noreply.github.com>
2021-11-02 23:48:48 -0400

    Create Propuesto 2.py

---
+++ Propuesto 2.py
@@ -0,0 +1,10 @@
+import math
+
+radio = float(input('radio'))
+
+Area = math.pi * radio**2
+longitud = 2 * math.pi * radio
+
+print(f"El area es: {Area}")
+print(f" La longitud de la circunferencia es:{longitud}")
+
