46f62d8ff740faaa49834e51a3aa92e14a3dfb2a
Valenvp14 <93169177+Valenvp14@users.noreply.github.com>
2021-11-02 23:10:58 -0400

    Create Propuesto 1.py

---
+++ Propuesto 1.py
@@ -0,0 +1,23 @@
+
+base = float(input("ingrese la base triangulo"))
+Altura = float(input("ingrese la altura"))
+
+Area = base * Altura / 2
+
+print(f"el area es igual a: {Area}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
