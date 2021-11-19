30c546fba3dc10e16a875a1e512c325cb24e9455
Valenvp14 <93169177+Valenvp14@users.noreply.github.com>
2021-11-19 13:33:07 -0400

    Create Continuacion ejercicio 2.1.py

---
+++ Continuacion ejercicio 2.1.py
@@ -0,0 +1,72 @@
+class Vehiculo:  # clase mas abstracta
+    # este es el constructor de la clase
+    def __init__(self, marca, modelo, anio):
+        self.marca = marca
+        self. modelo = modelo
+        self.anio = anio
+        print("Vehiculo Creado")
+
+    def encender(self):
+        print("Estoy listo para llevarte")
+
+    def arrancarmoto(self):
+        print("encendido y listo")
+
+class Automovil(Vehiculo):  # herencia simple: Automovil extiende a Vehiculo
+    __galonesGasolina = 0
+    transmisionManual = True
+
+    def encender(self):  # ejemplo de Polimorfismo - overriding
+        print("Listo para llevarte en este", self.modelo)
+
+    def arrancar(self):
+        print("raaaahh rahh!!")
+
+    def tocarCorneta(self):
+        print("beep beep!")
+
+    def echarGasolina(self, vGalones):  # Encapsulamiento - esto es un setter
+        if vGalones <= 1:
+            print("con un solo galon, no llegas a ninguna parte")
+        else:
+            self.__galonesGasolina = vGalones
+
+    def cuantaGasolinaHay(self):  # Encapsulamiento - esto es un getter
+        print("Ud. tiene:", self.__galonesGasolina, "galones de Gasolina")
+
+class moto(Vehiculo):
+    __rueda = 0
+
+    def prender(self):
+      print("Ready")
+
+    def arrancarmoto(self):
+        print("no tengo ruedas para llevarte en este", self.marca)
+
+    def comprarrueda(self, rueda):
+        if rueda <= 1:
+            print("con tres ruedas no puedes moverte")
+        else:
+            self.__rueda = rueda
+
+    def cuantasruedasnecesito(self):
+        print("usted necesita", self.__rueda, "ruedas para la moto")
+
+
+# instanciando la clase Vehiculo-Automovil
+miCarro = Automovil("Chevrolet", "Aveo", "2015")
+miCarro.encender()
+miCarro.arrancar()
+miCarro.tocarCorneta()
+miCarro.cuantaGasolinaHay()
+miCarro.echarGasolina(100)
+miCarro.cuantaGasolinaHay()
+
+mimoto = moto("CFMOTO", "BMW", "2021")
+mimoto.prender()
+mimoto.arrancarmoto()
+mimoto.cuantasruedasnecesito()
+mimoto.comprarrueda(4)
+mimoto.cuantasruedasnecesito()
+
+
